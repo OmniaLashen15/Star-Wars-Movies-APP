30621bb9446061c79b151b527448d2e3c813d1ca
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-08-30 18:50:38 +0200

    handling http errors

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler(){
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
+    setError(null); //to make sure that we clear any previous errors
+    try{
+      const response = await fetch('https://swapi.dev/api/films/');
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
+      const data = await response.json();
 
-    const transformedMovies = data.results.map(movieData=>{
+
+      const transformedMovies = data.results.map(movieData=>{
         return {
           id:movieData.episode_id,
           title:movieData.title,
@@ -23,6 +30,22 @@ function App() {
       setMovies(transformedMovies);
       setIsLoading(false);
 
+    } catch(error){
+      setError(error.message);
+      setIsLoading(false);
+    }
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if(movies.length > 0){
+    content = <MoviesList movies={movies} />;
+  }
+  if(error){
+    content = <p>{error}</p>;
+  }
+  if(isLoading){
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -30,11 +53,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading.....</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
